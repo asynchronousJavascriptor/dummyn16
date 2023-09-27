467556907a61fc380924df7d4335ca70ab6e67a2
Harsh Vandana Sharma <harshu8545@gmail.com>
2023-09-27 20:28:23 +0530

    commit of first

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var fs = require("fs");
+
+router.get('/', function(req, res, next) {
+  fs.readdir(`./files`, {withFileTypes: true}, function(err, files){
+    res.render("index", {files});
+  });  
+});
+
+router.get('/filecreate', function(req, res, next) {
+  fs.writeFile(`./files/${req.query.filename}`, "", function(err){
+    if(err) throw err;
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
