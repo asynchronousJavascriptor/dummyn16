cfdd496ffdf9ec4ce439f07acf7e918b73f65919
Harsh Vandana Sharma <harshu8545@gmail.com>
2023-10-03 16:44:46 +0530

    commit of 03 10

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,43 @@
 var express = require('express');
 var router = express.Router();
-var fs = require("fs");
+const fs = require("fs");
 
-router.get('/', function(req, res, next) {
-  fs.readdir(`./files`, {withFileTypes: true}, function(err, files){
+router.get("/", function(req, res){
+  fs.readdir("./files", {withFileTypes: true}, function(err, files){
     res.render("index", {files});
-  });  
+  })ś
 });
 
-router.get('/file/:filename', function(req, res, next) {
-  fs.readdir(`./files`, {withFileTypes: true}, function(err, files){
-    res.render("fileshow", {files});
-  });  
-});
-
-router.get('/foldercreate', function(req, res, next) {
-  fs.mkdir(`./files/${req.query.foldername}`, function(err){
-    if(err) throw err;
+router.get("/file/delete/:filename", function(req, res){
+  fs.unlink(`./files/${req.params.filename}`, function(err){
     res.redirect("/");
   })
 });
 
-router.get('/filecreate', function(req, res, next) {
-  fs.writeFile(`./files/${req.query.filename}`, "", function(err){
-    if(err) throw err;
+router.get("/folder/delete/:filename", function(req, res){
+  fs.rmdir(`./files/${req.params.filename}`, function(err){
     res.redirect("/");
-  });
+  })
+});
+
+router.get("/filecreate", function(req, res){
+  fs.writeFile(`./files/${req.query.filename}`, "", function(err){
+    res.redirect("/");
+  })
+});
+
+router.get("/foldercreate", function(req, res){
+  fs.mkdir(`./files/${req.query.foldername}`, function(err){
+    res.redirect("/");
+  })
+});
+
+router.get("/file/:filename", function(req, res){
+  fs.readdir("./files", {withFileTypes: true}, function(err, files){
+    fs.readFile(`./files/${req.params.filename}`, "utf8", function(err, data){
+      res.render("fileshow", {files, filename: req.params.filename, data});
+    })
+  })
 });
 
 module.exports = router;
